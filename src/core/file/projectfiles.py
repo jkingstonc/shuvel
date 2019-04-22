e7d425085fb0ce5634a502f28cc9d0b3a06d0f60
James Clarke <jkc200013@gmail.com>
2019-04-22 15:27:47 +0100

    Added project default directory variables.

---
+++ src/core/file/projectfiles.py
@@ -0,0 +1,7 @@
+# James Clarke
+# 22/04/2019
+
+# A class for helping locate project files when using the shovel command line app
+
+class ProjectFiles:
+    pass
---
+++ src/core/file/shuveldefaults.py
@@ -0,0 +1,11 @@
+# James Clarke
+# 18/04/2019
+
+# Contains all variables for locating directories in a shuvel project
+
+SHUVEL_ROOT="//"                        # Root directory for a shuvel project
+MUSEUM_STORE=SHUVEL_ROOT+"museum//"     # Directory for the museum directory {stores all nodes}
+RELIC_STORE=MUSEUM_STORE+"relics//"     # Directory for relic storage
+COLLECTION_STORE=RELIC_STORE            # Directory for collection storage
+STRATA_STORE=MUSEUM_STORE+"stratas//"   # Directory for strata storage
+
