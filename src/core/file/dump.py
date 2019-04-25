738b89b2110c4e6b4305c648e11fdb1d8bb4c333
James Clarke <jkc200013@gmail.com>
2019-04-25 16:04:20 +0100

    added relic names. added collection dumps.

--- src/core/file/dump.py
+++ src/core/file/dump.py
@@ -1,3 +1,8 @@
+# James Clarke
+# 25/04/2019
+
+# All verification is done before any of these functions are called
+
 from ..file.projectfiles import ProjectFiles
 from ..file.fileio import FileIO
 
@@ -8,10 +13,11 @@ class Dump:
     def dump_relic(relic,archive_dir):
         FileIO.write_string_overwride(archive_dir+relic.get_checksum_short(), relic.get_string_dump())
 
-    # Write a relic object to disk in the archive directory of a project
+    # Write a temp relic object to disk in the archive temp directory of a project
     @staticmethod
     def dump_temp_relic(relic,archive_dir):
         FileIO.write_string_overwride(archive_dir+relic.get_checksum_short(), relic.get_string_dump())
 
-    def dump_collection():
-        pass
+    # Write a collection object to disk in the archive directory of a project
+    def dump_collection(collection,archive_dir):
+        FileIO.write_string_overwride(archive_dir+collection.get_checksum_short(), checksum.get_string_dump())
--- src/core/nodes/relic.py
+++ src/core/nodes/relic.py
@@ -14,23 +14,27 @@ class Relic(Node):
     def __init__(self):
         super().__init__()
 
-        self._creation_date=None            # The date this relic was created
-        self._storage_data_contents="UNSET"  # Contains the raw data that is described in the origin file to be written to disk
+        self._creation_date=None                # The date this relic was created
+        self._name="DEFAULT"                    # Name associated with this relic
+        self._storage_data_contents="UNSET"     # Contains the raw data that is described in the origin file to be written to disk
     def __str__(self):
         return "Checksum: "+str(self.get_checksum_short())+"..., Creation-Date: "+str(self._creation_date)+", Storage-Data-Contents: "+str(self._storage_data_contents)
     
     # Set the checksum for this relic object
     def checksum_me(self):
-        self.checksum=Node.generate_checksum(str(self._creation_date)+str(self._storage_data_contents))
+        self.checksum=Node.generate_checksum(str(self._creation_date)+str(self._name)+str(self._storage_data_contents))
     # Set the creation date for this relic object
     def set_creation_date(self, creation_date=date.today()):
         self._creation_date=creation_date
+    # Set the name associated with this relic
+    def set_name(self, name):
+        self._name=name
     # Set the storage data contents for this relic object
     def set_storage_data_contents(self, contents):
         self._storage_data_contents=str(contents)
     # Dump the relic contents into a single string
     def get_string_dump(self):
-        return str(self._creation_date)+Relic.INFO_SEPERATOR+str(self._storage_data_contents)
+        return str(self._creation_date)+":"+str(self._name)+Relic.INFO_SEPERATOR+str(self._storage_data_contents)
 
 
 
